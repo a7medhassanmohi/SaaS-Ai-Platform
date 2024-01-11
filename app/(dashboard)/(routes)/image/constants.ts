ce58d8638a0d88323a123161abfdee201b6784f8
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-11 11:58:08 +0200

    remove some image amountOptions

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -12,22 +12,7 @@ export const formSchema = z.object({
       value: "1",
       label: "1 Photo"
     },
-    {
-      value: "2",
-      label: "2 Photos"
-    },
-    {
-      value: "3",
-      label: "3 Photos"
-    },
-    {
-      value: "4",
-      label: "4 Photos"
-    },
-    {
-      value: "5",
-      label: "5 Photos"
-    }
+  
   ];
   
   export const resolutionOptions = [
@@ -38,10 +23,6 @@ export const formSchema = z.object({
     {
       value: "512x512",
       label: "512x512",
-    },
-    {
-      value: "1024x1024",
-      label: "1024x1024",
     },
   ];
   
